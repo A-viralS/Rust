79519033a6ac17aec1dc1601c060aec8eb7bd613
Aviral <soniaviral14@gmail.com>
2024-02-21 18:36:47 +0530

    restructing folders

--- tut3/target/debug/deps/tut3-ef077295140ce1c1.d
+++
@@ -1,5 +0,0 @@
-/Users/aviralsoni/Desktop/blockchain/Rust/tut3/target/debug/deps/tut3-ef077295140ce1c1: src/main.rs
-
-/Users/aviralsoni/Desktop/blockchain/Rust/tut3/target/debug/deps/tut3-ef077295140ce1c1.d: src/main.rs
-
-src/main.rs:
--- tut3/target/debug/tut3.d
+++
@@ -1,1 +0,0 @@
-/Users/aviralsoni/Desktop/blockchain/Rust/tut3/target/debug/tut3: /Users/aviralsoni/Desktop/blockchain/Rust/tut3/src/main.rs
